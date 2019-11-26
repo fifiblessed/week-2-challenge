403c59b144803c3eb7f6b5dcc3fc69717f9abffb
fifiblessed <fifiblessed@gmail.com>
2019-11-26 17:38:27 +0300

    added some object assignment as well

--- week2.js
+++ week2.js
@@ -7,3 +7,9 @@ if(legability < 18){
 } else {
     console.log('adult');
 }
+//objects;
+let squares = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+for (i=0; i<16;i++){
+    Math.pow(squares[i],2);
+    console.log(squares[i]);
+}
